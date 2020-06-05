895b9a136e6c7d0753c301d4a253b20d191e5da2
willian emboava <w.emboava.o@gmail.com>
2020-06-05 00:39:12 -0300

    added main logo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Image } from "react-native";
 
 import { Container } from "./styles";
+import logoImg from "../../assets/images/logo.png"
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-
+      <Image source={logoImg} />
     </Container>
   )
 }
