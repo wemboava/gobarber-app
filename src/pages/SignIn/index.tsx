dd22d2c61b5e0bf2d88014b98a8900b98367a13e
willian emboava <w.emboava.o@gmail.com>
2020-06-05 14:29:22 -0300

    created input and button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Image } from "react-native";
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/images/logo.png';
+
 import { Container, Title } from "./styles";
-import logoImg from "../../assets/images/logo.png"
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => { console.log('deu') }}>Entrar</Button>
     </Container>
   )
 }
