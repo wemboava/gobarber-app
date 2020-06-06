27b0f29a5fdb092221e9337fb62edb7d4fad461b
willian emboava <w.emboava.o@gmail.com>
2020-06-06 16:12:35 -0300

    added unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -18,7 +20,14 @@ import {
 } from "./styles";
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const  navigation = useNavigation();
+
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, [])
+
 
   return (
     <>
@@ -30,10 +39,16 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button onPress={() => { console.log('deu') }}>Entrar</Button>
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >Entrar</Button>
+            </Form>
 
             <ForgotPassword onPress={() => { console.log('deu') }}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
